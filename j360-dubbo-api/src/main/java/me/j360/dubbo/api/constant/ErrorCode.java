fd24736f30d891efdbaa223d1b49da13a1d40d10
min_xu <xumin_wlt@163.com>
2017-03-15 20:12:37 +0800

    add new module

---
+++ j360-dubbo-api/src/main/java/me/j360/dubbo/api/constant/ErrorCode.java
@@ -0,0 +1,13 @@
+package me.j360.dubbo.api.constant;
+
+/**
+ * Package: me.j360.dubbo.base.api
+ * User: min_xu
+ * Date: 2017/3/15 下午8:04
+ * 说明：
+ */
+public class ErrorCode {
+
+    public static final int OBJECT_DELETE = -1;
+
+}
