4ad1df57ff68c6bfe1bec73aa6af4004848e3283
min_xu <xumin_wlt@163.com>
2017-05-17 10:40:42 +0800

    add Result dto

---
+++ j360-dubbo-api/src/main/java/me/j360/dubbo/api/model/result/UserBindResultDTO.java
@@ -0,0 +1,15 @@
+package me.j360.dubbo.api.model.result;
+
+import lombok.Data;
+import me.j360.dubbo.base.model.domian.BaseDO;
+
+/**
+ * Package: me.j360.dubbo.api.model.result
+ * User: min_xu
+ * Date: 2017/5/17 上午10:38
+ * 说明：
+ */
+@Data
+public class UserBindResultDTO extends BaseDO {
+
+}
---
+++ j360-dubbo-api/src/main/java/me/j360/dubbo/api/model/result/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Package: me.j360.dubbo.api.model.result
+ * User: min_xu
+ * Date: 2017/5/17 上午10:38
+ * 说明：
+ */
+package me.j360.dubbo.api.model.result;
