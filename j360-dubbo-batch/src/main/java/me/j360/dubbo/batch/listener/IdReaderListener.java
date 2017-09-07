ed689baf455ab58060bf9790ba2d2be53b9dce26
min_xu <xumin_wlt@163.com>
2017-09-07 11:55:10 +0800

    添加batch批处理案例+任务控制

---
+++ j360-dubbo-batch/src/main/java/me/j360/dubbo/batch/listener/IdReaderListener.java
@@ -0,0 +1,27 @@
+package me.j360.dubbo.batch.listener;
+
+import org.springframework.batch.core.ItemReadListener;
+
+/**
+ * Package: cn.paomiantv.batch.listener
+ * User: min_xu
+ * Date: 2017/8/8 下午2:21
+ * 说明：
+ */
+public class IdReaderListener implements ItemReadListener {
+
+    @Override
+    public void beforeRead() {
+
+    }
+
+    @Override
+    public void afterRead(Object item) {
+
+    }
+
+    @Override
+    public void onReadError(Exception ex) {
+
+    }
+}
